a2a91d5e8318a7cef8bc4711c864fde23916e525
pure <ppvadon@gmail.com>
2025-07-26 17:14:07 +0700

    css

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ let moon = document.getElementById("moon-icon");
 let themeBtn = document.getElementById("themeBtn");
 
 // set default theme
-document.body.classList.add("light");
 sun.style.display = "none";
 
 themeBtn.addEventListener("click", () => {
-  const isLight = document.body.classList.contains("light");
+  // check isDark while click the button
+  const isDark = document.body.classList.contains("dark");
 
-  // add class to body when click the botton
-  document.body.classList.toggle("light");
   document.body.classList.toggle("dark");
 
-  // show differece theme icon
-  if (isLight) {
-    sun.style.display = "";
-    moon.style.display = "none";
-  } else {
+  // display icon theme
+  if (isDark) {
     sun.style.display = "none";
     moon.style.display = "";
+  } else {
+    sun.style.display = "";
+    moon.style.display = "none";
   }
 });
+
